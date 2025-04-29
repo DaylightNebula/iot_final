c35e854051b0033187da84a8ba1e4c9366276ce8
DaylightNebula <daylightnebula1@gmail.com>
2025-04-28 20:52:43 -0600

    working on rewrite

--- iot_final_virtdriver/src/main.rs
+++ iot_final_virtdriver/src/main.rs
@@ -1,4 +1,4 @@
-use std::time::Duration;
+use std::{mem::transmute_copy, time::Duration};
 
 use serialport::SerialPort;
 
@@ -16,29 +16,51 @@ fn main() {
         .timeout(Duration::from_millis(10))
         .open().expect("Failed to open port");
 
+    for _ in 0 .. 4 {
+        let _ = port.write(&[0x00]);
+        let _ = port.flush();
+        std::thread::sleep(Duration::from_millis(10));
+    }
+
     loop {
         let _ = port.write(&[0x00]);
+        let _ = port.flush();
+
+        print!("\r");
 
         // wait until we have something to read
-        while port.bytes_to_read().unwrap_or(0) < 1 {}
+        while port.bytes_to_read().unwrap_or(0) < 12 {}
+        let total_bytes = port.bytes_to_read().unwrap_or(0);
 
-        // read angle
-        let angle_z: f32 = unsafe { std::mem::transmute(read_bytes::<4>(&mut port)) };
+        // read headers
+        let tick_length = read::<u32, 4>(&mut port);
+        let longest_length = read::<u32, 4>(&mut port);
+        print!("{:0.5} | {:05} | {:05}", total_bytes, tick_length, longest_length);
+
+        // read inputs
+        let angle = read::<f32, 4>(&mut port);
+
+        println!(" | {:>5.1}", angle);
 
         // read encoders
-        let num_encoders: i32 = unsafe { std::mem::transmute(read_bytes::<4>(&mut port)) };
-        if num_encoders > 4 { continue }
-        let mut encoders = Vec::<i16>::with_capacity(num_encoders as usize);
-        for _ in 0 .. num_encoders {
-            let rotation: i16 = unsafe { std::mem::transmute(read_bytes::<2>(&mut port)) };
-            encoders.push(rotation);
-        }
+        // let num_encoders: i32 = unsafe { std::mem::transmute(read_bytes::<4>(&mut port)) };
+        // if num_encoders > 4 { continue }
+        // let mut encoders = Vec::<i16>::with_capacity(num_encoders as usize);
+        // for _ in 0 .. num_encoders {
+        //     let rotation: i16 = unsafe { std::mem::transmute(read_bytes::<2>(&mut port)) };
+        //     encoders.push(rotation);
+        // }
 
-        println!("Angle {angle_z}, Encoders {encoders:?}");
+        // println!("Angle {angle_z}, Encoders {encoders:?}");
 
         // slow down
         std::thread::sleep(Duration::from_millis(4));
     }
+}
+
+fn read<O, const LEN: usize>(port: &mut Box<dyn SerialPort>) -> O {
+    assert_eq!(size_of::<O>(), LEN, "Size mismatch in read!");
+    unsafe { transmute_copy(&read_bytes::<LEN>(port)) }
 }
 
 fn read_bytes<const LEN: usize>(port: &mut Box<dyn SerialPort>) -> [u8; LEN] {
